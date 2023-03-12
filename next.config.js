73430449f3e70f353548d1c1f060e765a8c9b2da
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2023-03-12 15:56:32 +0700

    required by nextjs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: "http://localhost:8080/:path*"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
